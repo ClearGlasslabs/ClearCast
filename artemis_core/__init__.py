4b8da5446d8eaf8adff2b7cd9277f8584671f666
Desmond Otieno <77194670+ClearGlasslabs@users.noreply.github.com>
2026-08-03 14:17:44 -0400

    feat: add governed Artemis reference runtime

---
+++ artemis_core/__init__.py
@@ -0,0 +1,21 @@
+"""Governed reference runtime for ClearGlassInc Artemis."""
+
+from .runtime import (
+    ActionDecision,
+    ArtifactCandidate,
+    ArtifactRegistry,
+    FeedbackEvent,
+    MissionContext,
+    PolicyEngine,
+    TriageWorkflow,
+)
+
+__all__ = [
+    "ActionDecision",
+    "ArtifactCandidate",
+    "ArtifactRegistry",
+    "FeedbackEvent",
+    "MissionContext",
+    "PolicyEngine",
+    "TriageWorkflow",
+]
